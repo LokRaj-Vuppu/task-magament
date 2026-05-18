5459ba00f96324c6c0a61503fc6700d359cd2841
Raj Kumar <lokrajkumarvuppu@gmail.com>
2026-05-18 16:59:58 +0000

    env chnages

--- app/settings.py
+++ app/settings.py
@@ -3,7 +3,7 @@ from datetime import timedelta
 from pathlib import Path
 
 from celery.schedules import crontab
-from decouple import config
+# from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -184,8 +184,8 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = config("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
